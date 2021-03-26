31430127d2286aaccd150a257dbdf0ef75e43e45
Owner <owner@OwnernoMacBook-ea.local>
2021-03-26 14:28:59 +0900

    第一回アルゴリズム実技検定過去問a

--- atCoder_past201912_a.java
+++ atCoder_past201912_a.java
@@ -1,19 +1,18 @@
 import java.util.*;
 
 class myatcodertest{
-    private static int[] numChecker(){
-        Scanner sc = new Scanner( System.in );
-        String[] moji = sc.nextLine().split("");
-        //System.out.println(moji[1]);
-        int[] ans = new int[3];
+    private static int[] numChecker(){              //数字かどうかを判断するメソッド
+        Scanner sc = new Scanner( System.in );      //入力受け取り
+        String[] moji = sc.nextLine().split("");    //１文字ずつに分割
+        int[] ans = new int[3];                     
         for(int i=0;i<3;i++){
-            try{
+            try{                                    //数字なら数値に,そうでなければ例外処理
                 ans[i] = Integer.parseInt(moji[i]);
             }catch ( NumberFormatException ex ){
                 return null;
             }
         } 
-        return ans;
+        return ans;                                 
     }
 
 
@@ -22,9 +21,10 @@ class myatcodertest{
         nums = numChecker();
 
         if(nums == null){
-            System.out.println("error");
+            System.out.println("error");            //例外処理の場合
         }else{
             System.out.println(nums[0] * 200 + nums[1] * 20 + nums[2] * 2);
+                                                    //元の数を二倍にして出力
         }
     }
 }
